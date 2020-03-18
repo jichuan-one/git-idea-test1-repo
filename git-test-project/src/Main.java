a93f2f93dd1205625f220411ee7cd577b4b0ded7
jichuan <1648035697@qq.com>
2020-03-18 19:23:45 +0800

    修改之后推送到github远程仓库

--- git-test-project/src/Main.java
+++ git-test-project/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-
+        //hello test git，修改推送到github远程仓库
     }
 
     public void test(){
